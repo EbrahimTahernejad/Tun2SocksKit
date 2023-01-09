fde47b2d8b941b36edf48e4f47c597556c95bd03
Arror <hallo.maqiang@icloud.com>
2023-01-09 10:44:20 +0000

    Tun2SocksKit Release 1.0.0!!!

--- Package.swift
+++ Package.swift
@@ -1,1 +1,17 @@
+// swift-tools-version: 5.7
 
+import PackageDescription
+
+let package = Package(
+  name: "Tun2SocksKit",
+  products: [
+    .library(name: "Tun2SocksKit", targets: ["Tun2SocksKit"])
+  ],
+  targets: [
+    .binaryTarget(
+      name: "ClashKit",
+      url: "https://github.com/daemonomead/Tun2SocksKit/releases/download/1.0.0/Tun2SocksKit.xcframework.zip",
+      checksum: "63bf0f0acbcb6ca6bdbc7af73decb9218756fef8c61f95161bbe0c6434f994b1"
+    )
+  ]
+)
