0565491f8d753df98eea4c48c23af69a57d7934f
Arror <hallo.maqiang@icloud.com>
2023-03-01 05:10:16 +0000

    Tun2SocksKit Release 2.0.0!!!

--- Package.swift
+++ Package.swift
@@ -10,8 +10,8 @@ let package = Package(
   targets: [
     .binaryTarget(
       name: "Tun2SocksKit",
-      url: "https://github.com/daemonomead/Tun2SocksKit/releases/download/1.4.0/Tun2SocksKit.xcframework.zip",
-      checksum: "8db00c29c8e6a9cfa17d6913688a7c756b0975e9a96ea1906dc8ee24a2aec228"
+      url: "https://github.com/daemonomead/Tun2SocksKit/releases/download/2.0.0/Tun2SocksKit.xcframework.zip",
+      checksum: ""
     )
   ]
 )
