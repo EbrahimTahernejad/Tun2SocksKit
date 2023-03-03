257cce7c8296f8fb07beb1d057e2f2589ce8b517
Daemooon <hallo.maqiang@icloud.com>
2023-03-03 15:14:35 +0800

    Update Tunnel.swift

--- Sources/Tun2SocksKit/Tunnel.swift
+++ Sources/Tun2SocksKit/Tunnel.swift
@@ -4,7 +4,7 @@ import HevSocks5Tunnel
 public enum Tunnel {
     
     public static func startTunnel(with fileDescriptor: Int32, configFilePath: String) -> Int32 {
-        return hev_socks5_tunnel_main(configFilePath.path(percentEncoded: false), fileDescriptor)
+        return hev_socks5_tunnel_main(configFilePath.cString(using: .utf8), fileDescriptor)
     }
     
     public static func stop() {
