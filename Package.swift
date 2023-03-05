c632729a38e6f4be3814f3e20f5b5cd5ff5e05b0
Arror <hallo.maqiang@icloud.com>
2023-03-05 19:11:42 +0800

    Add Tun2SocksKitC

--- Package.swift
+++ Package.swift
@@ -6,15 +6,29 @@ let package = Package(
   name: "Tun2SocksKit",
   platforms: [.iOS(.v12)],
   products: [
-    .library(name: "Tun2SocksKit", targets: ["Tun2SocksKit"]),
-    .library(name: "HevSocks5Tunnel", targets: ["HevSocks5Tunnel"])
+    .library(
+        name: "Tun2SocksKit",
+        targets: ["Tun2SocksKit"]
+    ),
+    .library(
+        name: "Tun2SocksKitC",
+        targets: ["Tun2SocksKitC"]
+    ),
+    .library(
+        name: "HevSocks5Tunnel",
+        targets: ["HevSocks5Tunnel"]
+    )
   ],
   targets: [
-    .target(name: "Tun2SocksKit", dependencies: ["HevSocks5Tunnel"]),
+    .target(name: "Tun2SocksKitC"),
     .binaryTarget(
-      name: "HevSocks5Tunnel",
-      url: "https://github.com/daemooon/Tun2SocksKit/releases/download/2.1.1/HevSocks5Tunnel.xcframework.zip",
-      checksum: "ab1561d535f82f6efffb24076c547c342d4f79827a2add23063b19ffb6437f02"
+        name: "HevSocks5Tunnel",
+        url: "https://github.com/daemooon/Tun2SocksKit/releases/download/2.1.1/HevSocks5Tunnel.xcframework.zip",
+        checksum: "ab1561d535f82f6efffb24076c547c342d4f79827a2add23063b19ffb6437f02"
+    ),
+    .target(
+        name: "Tun2SocksKit",
+        dependencies: ["Tun2SocksKitC", "HevSocks5Tunnel"]
     )
   ]
 )
---
+++ Sources/Tun2SocksKitC/include/Tun2SocksKitC.h
@@ -0,0 +1,21 @@
+#ifndef Tun2SocksKitC_H
+#define Tun2SocksKitC_H
+
+#include <stdint.h>
+
+#define CTLIOCGINFO 0xc0644e03UL
+
+struct ctl_info {
+    u_int32_t   ctl_id;
+    char        ctl_name[96];
+};
+struct sockaddr_ctl {
+    u_char      sc_len;
+    u_char      sc_family;
+    u_int16_t   ss_sysaddr;
+    u_int32_t   sc_id;
+    u_int32_t   sc_unit;
+    u_int32_t   sc_reserved[5];
+};
+
+#endif /* Tun2SocksKitC_H */
