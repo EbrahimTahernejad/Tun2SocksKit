8fa29d3a7d6f2c30e79c4be393b772b92f80e7d0
Ebrahim Tahernejad <ebrahimtahernejad@gmail.com>
2023-03-31 19:55:54 +0330

    Add `@escaping` to completion handler

--- Sources/Tun2SocksKit/Tunnel.swift
+++ Sources/Tun2SocksKit/Tunnel.swift
@@ -3,7 +3,7 @@ import HevSocks5Tunnel
 
 public enum Socks5Tunnel {
     
-    public static func run(withConfig filePath: String, using fileDescriptor: Int32, completionHandler: (Int32) -> ()) {
+    public static func run(withConfig filePath: String, using fileDescriptor: Int32, completionHandler: @escaping (Int32) -> ()) {
         DispatchQueue.global(qos: .userInitiated).async { [completionHandler] () in
             let code = hev_socks5_tunnel_main(filePath.cString(using: .utf8), fileDescriptor)
             completionHandler(code)
